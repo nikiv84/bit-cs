0fb0c5f5f65fbc8b4141c5bfaed1cda6c01a7545
Bit <classroom@bgit.rs>
2017-10-09 15:15:36 +0200

    Switch - exercise 01

---
+++ switch/s01.js
@@ -0,0 +1,28 @@
+var dayOfTheWeek = 1;
+
+switch (dayOfTheWeek) {
+    case 1:
+        console.log("It's Monday!");
+        break;
+    case 2:
+        console.log("It's Tuesday!");
+        break;
+    case 3:
+        console.log("It's Wednesday!");
+        break;
+    case 4:
+        console.log("It's Thursday!");
+        break;
+    case 5:
+        console.log("It's Friday!");
+        break;
+    case 6:
+        console.log("It's Saturday!");
+        break;
+    case 7:
+        console.log("It's Sunday!");
+        break;
+    default:
+        console.log("Error! Enter a number between 1 and 7!");
+        break;
+}
