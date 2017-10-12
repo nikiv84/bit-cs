725338385fb8fdb9a81a898b78d3d2386f28a37b
Ivan Nikolić <inikolic84po@gmail.com>
2017-10-12 20:49:09 +0200

    Changez

--- fun_practical/f09.js
+++ fun_practical/f09.js
@@ -2,17 +2,12 @@ function isPrime(num) {
     if (isNaN(num) || num <= 1) {
         return "Please enter a valid number."
     }
-    if (num === 2) {
-        return true;
-    } else {
-        for (var i = 2; i < num; i++) {
-            if (num % i === 0) {
-                return num + " is a composite number."
-            }
+    for (var i = 2; i < num; i++) {
+        if (num % i === 0) {
+            return num + " is a composite number."
         }
-        return num + " is a prime number."
-
     }
+    return num + " is a prime number."
 }
 
-console.log(isPrime(19));
+console.log(isPrime(14));
